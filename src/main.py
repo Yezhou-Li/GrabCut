4d2d09728bc2470dc95dc6fdb72149004f9e6f69
Yezhou Li <yezoli@outlook.com>
2020-06-20 09:38:14 +0800

    Resize

--- src/main.py
+++ src/main.py
@@ -1,64 +1,31 @@
-import sys
-import datetime as dt 
-import cv2 as cv
 import numpy as np
-import pathlib
-import PyQt5.QtGui as qg 
-import PyQt5.QtWidgets as qw 
-import GrabCutSingle as gcs
-import GrabCutBatch as gcb
+import cv2
 
-CUR_DIR = pathlib.Path(__file__).parent.absolute()
-
-class MainWindow(qw.QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        # window setting
-        self.setGeometry(300, 300, 360, 270)
-        self.setWindowTitle('Background Vison Main Panel')
-
-        # component setting
-        self.singleButton = qw.QPushButton('Single File Mode', self)
-        self.batchButton = qw.QPushButton('Batch Mode', self)
-
-        # layout setting
-        self.mainWidget = qw.QWidget(self)
-        self.setCentralWidget(self.mainWidget)
-        self.layout = qw.QGridLayout(self.mainWidget)
-        self.layout.addWidget(self.singleButton, 0, 0, 1, 1)
-        self.layout.addWidget(self.batchButton, 0, 1, 1, 1)
-
-        # slot
-        self.singleButton.clicked.connect(self.openSingleMode)
-        self.batchButton.clicked.connect(self.openBatchMode)
-
-        self.show()
-
-    def openSingleMode(self):
-        self.gcSingle = gcs.SingleMode()
-        self.gcSingle.show()
-
-    def openBatchMode(self):
-        self.gcBatch = gcb.BatchMode()
-
-
-
-
-
-# if __name__ == "__main__":
-#     app = qw.QApplication(sys.argv)
-#     path = f'{CUR_DIR}/archive/eye.png'
-#     app.setWindowIcon(qg.QIcon(path))
-
-#     mw = MainWindow()
-#     sys.exit(app.exec_())
-
-app = qw.QApplication(sys.argv)
-mw = MainWindow()
-sys.exit(app.exec_())
-
+img = cv2.imread("alliance/hat.JPG")
+rows,cols,channels = img.shape
+cv2.imshow('girl',img)
+ 
+#转换hsv
+hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+cv2.imshow('hsv', hsv)
+lower_blue=np.array([0,0,0])
+upper_blue=np.array([0,0,255])
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
+cv2.imshow('Mask', mask)
+ 
+#腐蚀膨胀
+erode=cv2.erode(mask,None,iterations=1)
+cv2.imshow('erode',erode)
+dilate=cv2.dilate(erode,None,iterations=1)
+cv2.imshow('dilate',dilate)
+ 
+#遍历替换
+for i in range(rows):
+    for j in range(cols):
+        if dilate[i,j]==255:
+            img[i,j]=(0, 255, 0)#此处替换颜色，为BGR通道
+cv2.imshow('res',img)
+ 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.waitKey(1)
